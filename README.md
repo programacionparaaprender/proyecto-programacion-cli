52a27c802c996ab478af9c60a128ffb1ef25cacb
programacionparaaprender <yancel209@gmail.com>
2023-03-12 21:23:10 -0500

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # proyecto-programacion-cli
 Proyecto de prueba de un cli
+
+### cursos
+https://www.udemy.com/course/npm-librerias-node-con-js-typescript-angular-y-otras-herramientas/
+
+# paquetes npm
+npm install chalk@4.1.2 inquirer figlet shelljs@0.8.3
+
+npm install figlet@1.2.1
+npm install inquirer@6.2.2
+
+### repositorios
+
+https://github.com/SBoudrias/Inquirer.js/
+https://github.com/shelljs/shelljs
+https://github.com/patorjk/figlet.js/
+http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+https://github.com/chalk/chalk
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const shelljs = require('shelljs');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+
+ficheroOK = filepath => {
+    console.log(
+        chalk.white.bgGreen.bold(
+            `¡Muy bien! Fichero correctamente creado en el directorio ${filepath}`
+        )
+    );
+}
+const crearFichero = (nombreFichero, extension) => {
+    const pathFichero = `${process.cwd()}/${nombreFichero}.${extension}`;
+    shelljs.touch(pathFichero);
+    return pathFichero;
+}
+const hacerPreguntas = () => {
+    const preguntas = [
+        {
+            name: "FICHERO",
+            type: "input",
+            message: "¿Cómo se va a llamar tu fichero? (sin la extensión)"
+        },
+        {
+            name: "EXTENSION",
+            type: "list",
+            message: "¿Qué extensión tiene tu fichero",
+            choices: [".rb", ".js", ".kt", ".java", ".ts", ".php"],
+            filter: function(val) {
+                return val.split(".")[1];
+            }
+        }
+    ];
+    return inquirer.prompt(preguntas);
+}
+const iniciar = () => {
+    console.log(
+        chalk.green(
+            figlet.textSync( "Creador Ficheros", {
+                font: "Bubble",
+                horizontalLayout: "default",
+                verticalLayout: "default"
+            })
+        )
+    );
+};
+const ejecutar = async () => {
+    // Mostrar la información de la librería en la cabecera, el título con figlet
+    iniciar();
+    // Preguntas necesarias para crear el fichero, es decir, el nombre y la extensión
+    const respuestas = await hacerPreguntas();
+    const { FICHERO, EXTENSION } = respuestas;
+    console.log(respuestas);
+    // Creamos el fichero
+    const pathFichero = crearFichero(FICHERO, EXTENSION);
+    // Añadimos mensaje que fichero se ha creado correctamente
+    ficheroOK(pathFichero);
+};
+
+
+ejecutar();
